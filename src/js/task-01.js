2dcaa5a6d9b94909bbe1dcd11f7132875480e78a
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-04-12 21:59:13 +0300

    12.04.1

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const categotiesLenghtEl = categories.children.length;
+
+console.log(`У списку ${categotiesLenghtEl} категорії`);
+
+const items = document.querySelectorAll(".item");
+
+console.log(items.inerHTML);
