7be78dbf01ac289e894f6b51808e845f721292ab
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-04-18 00:55:02 +0300

    18.04.1

--- js/task-08.js
+++ js/task-08.js
@@ -1,24 +1,27 @@
 const opions = ["#757575", "#2196F3", "#3F5185"];
 const inputEl = document.querySelector("input");
 const buttonEl = document.querySelectorAll("button");
+console.log(buttonEl);
 const boxEl = document.querySelectorAll("div");
 
-inputEl.addEventListener("change", onAmount);
+/* inputEl.addEventListener("change", onAmount);
 
 function onAmount(event) {
   event.currentTarget.value;
   return;
-}
+} */
 
 buttonEl[0].addEventListener("click", createBoxes);
 function createBoxes(amount) {
-  for (let i = 1; i < amount; i += 1) {
+  const array = [];
+  for (let i = 1; i < 6; i += 1) {
     const divElem = document.createElement("div");
     const randomEl = Math.floor(Math.random() * opions.length);
     const opponentMove = opions[randomEl];
     divElem.style.backgroundColor = opponentMove;
     divElem.style.width = "30px";
     divElem.style.height = "30px";
-    boxEl[1].append(divElem);
+    array.push(divElem);
   }
+  boxEl[1].append(...array);
 }
