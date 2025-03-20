da90ae2f6abf2bd506fd0207e2ecb1a44581d879
Alina Prystynska <pristinskayaalina9@gmail.com>
2025-03-20 18:31:28 +0000

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,6 @@
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 const categories = document.querySelectorAll('ul#categories li.item');
-
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach(category => {
